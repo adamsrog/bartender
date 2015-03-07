0e39a598feecc394ce44f8c06cfb202b1a3fc906
Roger Adams <adamsrog04@gmail.com>
2015-03-06 23:48:18 -0800

    created equipment model, fixed warning in ingredient model, setup CSP

--- config/environment.js
+++ config/environment.js
@@ -16,7 +16,17 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-    }
+    },
+
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src': "'self' 'unsafe-inline' 'unsafe-eval' 'google-analytics.com'",
+      'font-src': "'self'",
+      'connect-src': "'self'",
+      'img-src': "'self' *",
+      'style-src': "'self' 'unsafe-inline'",
+      'report-uri': "'none'"
+    },
   };
 
   if (environment === 'development') {
