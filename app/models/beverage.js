ccc1d5e316a78a41883f8ab2b022fa878759172c
Roger Adams <adamsrog04@gmail.com>
2015-03-08 18:13:25 -0700

    experimenting combining steps and amounts

--- app/models/beverage.js
+++ app/models/beverage.js
@@ -5,9 +5,22 @@ var Beverage = DS.Model.extend({
   type: DS.attr(),
   container: DS.attr(),
   description: DS.attr(),
+  amounts: DS.attr(),
   ingredients: DS.hasMany('ingredient', {async: true}),
   steps: DS.attr(),
-  tags: DS.attr()
+  tags: DS.attr(),
+
+  combined: Ember.computed('amounts', 'ingredients', function() {
+  	var amounts = this.get('amounts');
+  	var ingredients = this.get('ingredients.content');
+  	var combined = [];
+  	var count = 0;
+  	ingredients.forEach(function(ingredient) {
+  		combined.push(amounts[count] + ' ' + ingredient.get('name'));
+  		count++;
+  	});
+  	return combined;
+  })
 });
 
 Beverage.reopenClass({
@@ -18,11 +31,12 @@ Beverage.reopenClass({
 			type: 'cocktail',
 			container: 'cocktail-glass',
 			description: '',
+			amounts: ['1.5 oz','.75 oz','.75 oz','2 dashes','1'],
 			ingredients: ['gin','lillet-blanc','orange-juice','angostura-bitters','maraschino-cherry'],
 			steps: ['Shake the liquid ingredients vigorously with ice.','Strain into a chilled cocktail glass.','Garnish with the cherry.'],
-			tags: []
+			tags: [],
 		}
 	]
-})
+});
 
 export default Beverage;
