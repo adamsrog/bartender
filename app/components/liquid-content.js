1f66267f16107516bc6879f799a4a3f612364864
Roger Adams <adamsrog04@gmail.com>
2015-04-02 22:15:06 -0700

    update equipment images, show them in beverage view

--- app/components/liquid-content.js
+++ app/components/liquid-content.js
@@ -3,7 +3,10 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	// https://color.adobe.com/Salmon-on-Ice-color-theme-2291686
-	palette: ['#3E454C', '#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66'],
+	// palette: ['#3E454C', '#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66'],
+
+	// http://flatuicolors.com/
+	palette: ['#16a085', '#27ae60', '#2980b9', '#8e44ad', '#2c3e50', '#f39c12', '#d35400', '#c0392b', '#bdc3c7', '#7f8c8d'],
 
 	height: 400,
 	width: 400,
