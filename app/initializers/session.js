8b2d90f20aeea84bc1f49c5548c80c11233f565b
Roger Adams <adamsrog04@gmail.com>
2015-04-25 16:57:33 -0700

    users now persist in firebase, write convenience function to parse data based on provider

--- app/initializers/session.js
+++ app/initializers/session.js
@@ -1,19 +1,76 @@
 import Ember from 'ember';
 import Firebase from 'firebase';
 
+// convenience method for getting user's information based on the provider
+function parseAuthData(authData) {
+	var parsedData = {};
+	switch(authData.provider) {
+
+		case 'facebook':
+			parsedData.provider = authData.provider;
+			parsedData.id = authData.facebook.id;
+			parsedData.displayName = authData.facebook.displayName;
+			parsedData.gender = authData.facebook.cachedUserProfile.gender;
+			parsedData.language = authData.facebook.cachedUserProfile.locale;
+			parsedData.imageThumbUrl = authData.facebook.cachedUserProfile.picture.data.url;
+			parsedData.website = authData.facebook.cachedUserProfile.link;
+			return parsedData;
+
+		case 'github':
+			parsedData.provider = authData.provider;
+			parsedData.id = authData.github.id;
+			parsedData.username = authData.github.username;
+			parsedData.displayName = authData.github.displayName;
+			parsedData.description = authData.github.cachedUserProfile.bio;
+			parsedData.email = authData.github.email;
+			parsedData.company = authData.github.cachedUserProfile.company;
+			parsedData.location = authData.github.cachedUserProfile.location;
+			parsedData.imageThumbUrl = authData.github.cachedUserProfile.avatar_url;
+			parsedData.website = authData.github.cachedUserProfile.html_url;
+			return parsedData;
+
+		case 'google':
+			parsedData.provider = authData.provider;
+			parsedData.id = authData.google.id;
+			parsedData.displayName = authData.google.displayName;
+			parsedData.gender = authData.google.cachedUserProfile.gender;
+			parsedData.language = authData.google.cachedUserProfile.locale;
+			parsedData.imageThumbUrl = authData.google.cachedUserProfile.picture;
+			parsedData.website = authData.google.cachedUserProfile.link;
+			return parsedData;
+
+		case 'twitter':
+			parsedData.provider = authData.provider;
+			parsedData.id = authData.twitter.id;
+			parsedData.username = authData.twitter.username;
+			parsedData.displayName = authData.twitter.displayName;
+			parsedData.description = authData.twitter.cachedUserProfile.description;
+			parsedData.location = authData.twitter.cachedUserProfile.location;
+			parsedData.language = authData.twitter.cachedUserProfile.lang;
+			parsedData.imageThumbUrl = authData.twitter.cachedUserProfile.profile_image_url_https || authData.twitter.cachedUserProfile.profile_image_url;
+			parsedData.website = authData.twitter.cachedUserProfile.url;
+			return parsedData;
+	}
+}
+
 var session = Ember.Object.extend({
 	ref: new Firebase('https://fiery-torch-7549.firebaseio.com'),
 
 	addFirebaseCallback: function() {
 		var session = this;
+		var ref = this.get('ref');
 
-		this.get('ref').onAuth(function(authData) {
+		ref.onAuth(function(authData) {
 			if (authData) {
+				var user = parseAuthData(authData);
 				session.set('isAuthenticated', true);
+				session.set('user', user);
+				ref.child('users').child(authData.uid).set(user);
 			} else {
 				session.set('isAuthenticated', false);
 			}
 		});
+		
 	}.on('init'),
 
 	loginFacebook: function() {
