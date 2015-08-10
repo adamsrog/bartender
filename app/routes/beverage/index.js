a84fe87e148deb17ba3f274f487178d4ceffa118
Roger Adams <adamsrog04@gmail.com>
2015-08-09 19:41:55 -0700

    fix find -> findAll deprecation

--- app/routes/beverage/index.js
+++ app/routes/beverage/index.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model: function() {
-		return this.store.find('beverage');
+		return this.store.findAll('beverage');
 	}
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,8 +3,8 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model: function() {
 		return Ember.RSVP.hash({
-			beverages: this.store.find('beverage'),
-			ingredients: this.store.find('ingredient')
+			beverages: this.store.findAll('beverage'),
+			ingredients: this.store.findAll('ingredient')
 		});
 	}
 });
