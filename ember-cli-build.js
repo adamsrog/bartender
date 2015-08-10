f70c89f0494e5338944b79284a875cf78d891c04
Roger Adams <adamsrog04@gmail.com>
2015-08-09 18:03:42 -0700

    update npm deps

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,7 +3,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    fingerprint: {
+      exclude: ['assets/images']
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
@@ -18,6 +20,11 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  
+  app.import({
+    development: app.bowerDirectory + '/d3/d3.js',
+    production: app.bowerDirectory + '/d3/d3.min.js'
+  });
 
   return app.toTree();
 };
