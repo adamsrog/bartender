f3e90ee7ca010107865036c1cca3c5df5c248db5
Roger Adams <adamsrog04@gmail.com>
2015-08-13 20:39:03 -0700

    several form fixes

--- app/controllers/beverage/new.js
+++ app/controllers/beverage/new.js
@@ -1,4 +1,34 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	amounts: [{amount: 1, measurement: 'oz'}],
+	beverage_id: '',
+	description: '',
+	equipment: '',
+	ingredients: Ember.computed('ingredientsArray', function() {
+
+	}),
+	ingredientsArray: [{amount:1, measurement:'oz', name:''}],
+	name: '',
+	steps: [''],
+	type: '',
+
+	actions: {
+		addIngredient: function() {
+			var ingredients = this.get('ingredientsArray');
+			ingredients.pushObject({amount: '1', measurement: 'oz', name: ''});
+		},
+		removeIngredient: function(index) {
+			var ingredients = this.get('ingredientsArray');
+			ingredients.removeAt(index);
+		},
+		addStep: function() {
+			var steps = this.get('steps');
+			steps.addObject(steps.length);
+		},
+		removeStep: function(index) {
+			var steps = this.get('steps');
+			steps.removeAt(index);
+		}
+	}
 });
--- app/routes/beverage/new.js
+++ app/routes/beverage/new.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model: function() {
+		return Ember.RSVP.hash({
+			equipment: this.store.findAll('equipment'),
+			ingredients: this.store.findAll('ingredient')
+		});
+	}
 });
